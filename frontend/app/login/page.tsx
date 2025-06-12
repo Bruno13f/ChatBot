0559be59327312bd0274bda8f180ec348cc7b7ad
Bruno Ferreira <122650083+Bruno13f@users.noreply.github.com>
2025-06-12 21:05:31 +0100

    chore: add app name to login/signup pages

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -5,12 +5,13 @@ import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
   return (
-        <>
-        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-            <div className="w-full max-w-sm">
-            <LoginForm />
-            </div>
+    <div className="flex min-h-svh w-full items-center justify-center px-6 pb-20 md:p-10">
+      <div className="w-full max-w-sm">
+        <div className="mb-6 text-center">
+          <h1 className="text-4xl font-semibold">Chatbot</h1>
         </div>
-        </>
+        <LoginForm />
+      </div>
+    </div>
   )
 }
