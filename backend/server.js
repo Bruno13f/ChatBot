7e6abf0702552e064be363c401c6bd68a2cc6968
Diogo Guerra <63842251+diogoguerra03@users.noreply.github.com>
2025-06-14 11:26:31 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // endereço do frontend
+    origin: process.env.FRONTEND_URI || "http://localhost:3000", // endereço do frontend FRONTEND_URI
     methods: ["GET", "POST"],
     credentials: true,
   },
